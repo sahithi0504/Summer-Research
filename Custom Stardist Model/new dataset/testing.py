a033501d46e5c37b5857b0e6fd253322af2928ee
Skylar Wilson <angelswilson@outlook.com>
2023-07-17 14:09:20 -0500

    updated testing.py
    code now shows actual count vs predicted count in the pictures it produces. also, it updates the output pictures to be a higher resolution.

--- Custom Stardist Model/new dataset/testing.py
+++ Custom Stardist Model/new dataset/testing.py
@@ -1,6 +1,7 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 import numpy as np
 import matplotlib
+import os
 matplotlib.rcParams["image.interpolation"] = 'none'
 import matplotlib.pyplot as plt
 from glob import glob
@@ -11,6 +12,9 @@ from stardist.models import StarDist2D
 
 np.random.seed(6)
 lbl_cmap = random_label_cmap()
+
+image_dir = "/scratch/awil743/modeltesting/testing"
+file_names = sorted(os.listdir(image_dir))
 
 X = sorted(glob("/scratch/awil743/modeltesting/testing/*.tif"))
 X = list(map(imread,X))
@@ -27,6 +31,12 @@ def example(model, i, show_dist=True):
     # Count the number of unique labels (subtract 1 to exclude the background label 0)
     num_objects = len(np.unique(labels)) - 1
 
+    # Get the file name for the current image
+    file_name = file_names[i]
+
+    # Get the actual count from the file name
+    actual_count = int(file_name.split('count')[0])
+
     plt.figure(figsize=(13, 10))
     img_show = img if img.ndim == 2 else img[..., 0]
     coord, points, prob = details['coord'], details['points'], details['prob']
@@ -35,14 +45,14 @@ def example(model, i, show_dist=True):
     a = plt.axis()
     _draw_polygons(coord, points, prob, show_dist=show_dist)
     plt.axis(a)
-    plt.title(f'Number of Objects: {num_objects}', fontsize=16)  # Display the number of objects
+    plt.title(f'Predicted Objects: {num_objects}\nActual Objects: {actual_count}', fontsize=16)
     
     plt.subplot(122); plt.imshow(img_show, cmap='gray'); plt.axis('off')
     plt.imshow(labels, cmap=lbl_cmap, alpha=0.5)
     plt.tight_layout()
     
     # Save the figure to your specified directory
-    plt.savefig(f'/scratch/awil743/modeltesting/figure_{i}.png')  # Modify the filename as needed
+    plt.savefig(f"/scratch/awil743/modeltesting/figure_{i}.png", dpi=400)  # Modify the filename as needed
 
-for i in range(3):
+for i in range(4):
     example(model, i)
